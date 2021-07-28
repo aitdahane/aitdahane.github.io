28a0dc8509ddbb003a7f7aa58669ab5c4d3838a7
aitdahane <mohamed.aitdahane@gmail.com>
2021-07-28 21:42:09 +0100

    Update style

--- scripts/app.js
+++ scripts/app.js
@@ -3,12 +3,19 @@
 })();
 
 function navigateToPage(pageId) {
-    const pageElement = document.getElementById(pageId);
     const allPagesElements = document.getElementsByClassName('section');
     Array.prototype.forEach.call(allPagesElements, (element) => {
         element.classList.add('is-disabled');
     });
+    const pageElement = document.getElementById(pageId);
     pageElement.classList.remove('is-disabled');
+
+    const allLinkElements = document.getElementsByClassName('nav-link');
+    Array.prototype.forEach.call(allLinkElements, (element) => {
+        element.classList.remove('is-active');
+    });
+    const btnLinkElement = document.getElementById(`${pageId}-nav-link`);
+    btnLinkElement.classList.add('is-active');
 }
 
 function sendMessage() {
