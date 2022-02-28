e5be9c24a5685d3bd3b29eafd88a7dd8977d029a
aitdahane <mohamed.ad@padoa-group.com>
2022-02-28 23:22:42 +0100

    Update portfolio

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,3 @@
-(function() {
-    emailjs.init("user_WLKz5YYQdsHP8G4EUWHSL");
-})();
-
 function navigateToPage(pageId) {
     const allPagesElements = document.getElementsByClassName('section');
     Array.prototype.forEach.call(allPagesElements, (element) => {
@@ -18,31 +14,7 @@ function navigateToPage(pageId) {
     btnLinkElement.classList.add('is-active');
 }
 
-function sendMessage() {
-    const formElement = document.getElementById('contactForm');
-    const getFormValue = (key) => formElement.elements[key].value;
-    const email = getFormValue('email');
-    const message = getFormValue('message');
-    /*
-    emailjs.send('contact_service', 'template_5mk1l0u', { email, message })
-        .then(function() {
-            displaySuccessToast();
-            formElement.reset();
-        }, function(error) {
-            displayErrorToast(error);
-        });
-    }
-    */
-}
-
 function openUrl(url) {
     window.open(url, '_blank')
 }
 
-function displaySuccessToast() {
-    console.log('SUCCESS!');
-}
-
-function displayErrorToast(error) {
-    console.log('FAILED...', error);
-}
